12b88a53fcf8ee37298105d9b2b641edda8d7ec1
Eric Palace <epalace510@gmail.com>
2013-02-06 22:11:59 -0500

    Fleshed out the cp command, allowing for copying between buckets. Can now make, cd, remove, and ls for buckets. An expiration time can be set for objects (in days). Can also check for the existance of a bucket and see if you have permissions on a bucket.

--- s3Shell.rb
+++ s3Shell.rb
@@ -6,22 +6,28 @@ AWS.config(
     :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
 )
-s3=AWS::S3.new
+@s3=AWS::S3.new
 
-#file = ARGV.first
-bucket = s3.buckets['EP_TEST_BUCKET_1']
+bucket = @s3.buckets['EP_TEST_BUCKET_1']
 dbucket=nil
+def bucket_check(bucket)
+  test_bucket=@s3.buckets[bucket]
+  return test_bucket.exists?
+end
+#file = ARGV.first
 while true
   #command is self explanatory. param1 and param2 are used for different things, depending on the command.
   #if there aren't enough values in gets, then the unused params are left nil.
   command, param1, param2 = gets.split
   command.chomp!
+  file=nil
+  dbucket=nil
   #Uploads the given file to the currently selected bucket.
   if(command=="upload")
     file=param1
     file.chomp!
     bucket.objects[file].write(Pathname.new(file))
-   # AWS::S3::S3Object.write(Pathname.new(file))
+    # AWS::S3::S3Object.write(Pathname.new(file))
     puts "File " + file + " successfully uploaded!"
     puts file + " can be accessed at "
     puts bucket.objects[file].url_for(:read)
@@ -64,11 +70,109 @@ while true
         end
       end
       puts "File " +file+ " written."
+    elsif(bucket_check(dbucket))
+      puts 'The destination bucket does not exits.'
     elsif(!bucket.objects[file].exists?)
       puts "The file does not exist."
     else
       dbucket.chomp!
-      puts "I hope this doesn't show up."
+      bucket.objects[file].copy_to(file,{:bucket=>@s3.buckets[dbucket]})
+      puts 'File ' + file + ' copied to ' + dbucket
+    end
+  end
+  if(command=="pwd")
+    puts bucket.name
+  end
+  if(command=="cd")
+    dbucket=param1
+    dbucket.chomp!
+    unless(bucket_check(dbucket))
+      bucket=@s3.buckets[dbucket]
+    else
+      puts 'No such bucket.'
+    end
+  end
+  if(command=="mkdir")
+    dbucket=param1
+    dbucket.chomp!
+    dbucket.downcase
+    unless(bucket_check(dbucket))
+      @s3.buckets.create(dbucket)
+      puts 'Bucket ' + dbucket + ' created.'
+    else
+      puts 'Bucket already exists. Cannot create duplicate buckets.'
+    end
+  end
+  if(command=="lsbkt")
+    @s3.buckets.each do |dbucket|
+      puts dbucket.name
+    end
+  end
+  if(command=="rmdir")
+    dbucket=param1
+    dbucket.chomp!
+    if(bucket_check(dbucket))
+      puts 'Are you sure you want to delete the bucket' + dbucket  + ' and all its contents? (y/n)'
+      while true
+        check=gets
+        check.chomp!
+        if(check=="y")
+          dbucket = @s3.buckets[dbucket]
+          dbucket.clear!
+          dbucket.delete
+          puts "Bucket deleted."
+          break
+        elsif(check=="n")
+          puts "Operation aborted."
+          break
+        else
+          puts "Command not recognized"
+        end
+      end
+    else
+      puts 'Bucket does not exist, so cannot be deleted.'
+    end
+  end
+  if(command=="exists?")
+    dbucket=param1
+    dbucket.chomp!
+    puts bucket_check(dbucket)
+  end
+  if(command=="permission?")
+    dbucket=param1
+    dbucket.chomp!
+    unless(bucket_check(dbucket))
+      puts 'Bucket does not exist.'
+    else
+      begin
+        aclTest=@s3.buckets[dbucket].acl
+        puts 'You have permissions for this bucket.'
+      rescue AWS::S3::Errors::AccessDenied => e
+        puts 'You do not have access to this bucket.'
+      end
+    end
+  end
+  if(command=="SetExpire")
+    file = param1
+    file.chomp!
+    expireDate = param2
+    expireDate.chomp!
+    begin
+      if(bucket.objects[file].exists?)
+        bucket.lifecycle_configuration.update do
+          add_rule(file, :expiration_time => expireDate.to_i)
+        end
+      else
+        puts 'File does not exist.'
+      end
+    rescue AWS::S3::Errors::InvalidRequest => e
+      if(bucket.objects[file].exists?)
+        bucket.lifecycle_configuration.replace do
+          add_rule(file, :expiration_time => expireDate.to_i)
+        end
+      else
+        puts 'File does not exist.'
+      end
     end
   end
   if(command=="exit")
